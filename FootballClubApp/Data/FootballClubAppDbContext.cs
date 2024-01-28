117eef59cb782e6f1961569bfe859fdea1fc3d62
OBJECTIVITY\zmacinska <zmacinska@objectivity.co.uk>
2024-01-28 11:17:05 +0100

    move connection string to appsettings.json

--- FootballClubApp/Data/FootballClubAppDbContext.cs
+++ FootballClubApp/Data/FootballClubAppDbContext.cs
@@ -2,12 +2,19 @@
 
 using Microsoft.EntityFrameworkCore;
 using FootballClubApp.Data.Entities;
+using Microsoft.Extensions.Configuration;
 
 public class FootballClubAppDbContext : DbContext
 {
-    private readonly string _connectionString = @"Server=(local)\SQLEXPRESS;Database=FootballAppDb;Trusted_Connection=True";
     public DbSet<Player> Players => Set<Player>();
     public DbSet<Opponent> Opponents => Set<Opponent>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        IConfiguration configuration = configurationBuilder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -42,26 +49,4 @@ public class FootballClubAppDbContext : DbContext
             .IsRequired()
             .HasMaxLength(50);
     }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer(_connectionString);
-    }
-
-    //modelBuilder.Entity<Player>()
-    //    .HasData(
-    //        new Player { Id = 1, FirstName = "Ansu", LastName = "Fati", Number = "10", Position = Position.Attacker },
-    //        new Player { Id = 2, FirstName = "Gerard", LastName = "Piqué", Number = "3", Position = Position.Defender },
-    //        new Player { Id = 3, FirstName = "Marc-André", LastName = "ter Stegen", Number = "1", Position = Position.Goalkeeper },
-    //        new Player { Id = 4, FirstName = "Pablo", LastName = "Gavi", Number = "30", Position = Position.Midfielder },
-    //        new Player { Id = 5, FirstName = "Sergio", LastName = "Busquets", Number = "5", Position = Position.Midfielder, IsCaptain = true },
-    //        new Player { Id = 6, FirstName = "Nico", LastName = "Gonzalez", Number = "14", Position = Position.Midfielder }
-    //    );
-
-    //    modelBuilder.Entity<Opponent>()
-    //        .HasData(
-    //            new Opponent { Id = 1, TeamName = "Real Madrid", Competition = Competition.LaLiga },
-    //            new Opponent { Id = 2, TeamName = "Atletico Madrid", Competition = Competition.LaLiga },
-    //            new Opponent { Id = 3, TeamName = "SSC Napoli", Competition = Competition.EuropaLeague },
-    //            new Opponent { Id = 4, TeamName = "Athletic Bilbao", Competition = Competition.LaLiga }
-    //        );
 }
